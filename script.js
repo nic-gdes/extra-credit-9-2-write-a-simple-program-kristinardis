6fe02ef1177e178d7ef52007d72f45d9c9e52508
Kkri McILvenna <kamcilvenna@nic.edu>
2023-10-19 16:34:58 -0700

    practice 2

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+var name = "class";
+
+if (name === "five") {
+    alert("execute <p>");
+}
+
+if (name === "four") {
+    alert("execute <p>");
+}
+
+if (name === "three") {
+    alert("execute <p>");
+}
+
+if (name === "two") {
+    alert("execute <p>");
+}
+
+if (name === "one") {
+    alert("execute <p>");
+}
